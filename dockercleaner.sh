b368011028618e584312eac655ec14d27c2e4212
Andrew Nicols <andrew@nicols.co.uk>
2017-11-03 09:12:51 +0800

    dockercleaner: Make this not suck

--- dockercleaner.sh
+++ dockercleaner.sh
@@ -5,16 +5,4 @@
 # instances which have existed but not removed
 #############################################
 
-# Get all exited docker instances.
-docker ps -a | grep "Exited" | awk '{print $1}' | sed 's/ //' > ~/dockerexit.txt
-
-# Run though all and remove them
-IFS='
-'
-exec 4<~/dockerexit.txt           # opening  file via descriptor
-while read DOCKERID <&4; do
- docker rm $DOCKERID
-done
-
-# Remove the file
-rm ~/dockerexit.txt
+docker rm $(docker ps -a --filter 'exited=0' -q)
