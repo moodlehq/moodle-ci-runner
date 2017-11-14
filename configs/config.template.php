affd3c837dcb2bcdb711f026f7ad4749920f1ccc
Andrew Nicols <andrew@nicols.co.uk>
2017-11-15 07:59:11 +0800

    run: Add new job runner

---
+++ configs/config.template.php
@@ -0,0 +1,77 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = getenv('DBTYPE');
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = getenv('DBHOST');
+$CFG->dbname    = getenv('DBNAME');
+$CFG->dbuser    = getenv('DBUSER');
+$CFG->dbpass    = getenv('DBPASS');
+$CFG->prefix    = 'm_';
+$CFG->dboptions = ['dbcollation' => getenv('DBCOLLATION')];
+
+$CFG->wwwroot   = 'http://host.name';
+$CFG->dataroot  = '/var/www/moodledata';
+$CFG->admin     = 'admin';
+$CFG->directorypermissions = 0777;
+
+// Debug options - possible to be controlled by flag in future..
+$CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
+$CFG->debugdisplay = 1;
+$CFG->debugstringids = 1; // Add strings=1 to url to get string ids.
+$CFG->perfdebug = 15;
+$CFG->debugpageinfo = 1;
+$CFG->allowthemechangeonurl = 1;
+$CFG->passwordpolicy = 0;
+
+$CFG->phpunit_dataroot  = '/var/www/phpunitdata';
+$CFG->phpunit_prefix = 't_';
+
+$CFG->behat_wwwroot   = 'http://' . getenv('WEBSERVER');
+$CFG->behat_dataroot  = '/var/www/behatdata';
+$CFG->behat_prefix = 'b_';
+$CFG->behat_profiles = array(
+    'default' => array(
+        'browser' => getenv('BROWSER'),
+        'wd_host' => getenv('SELENIUMURL') . '/wd/hub',
+    ),
+);
+$CFG->behat_faildump_path = '/shared';
+
+define('PHPUNIT_LONGTEST', true);
+
+if (!empty(getenv('MOODLE_DOCKER_PHPUNIT_EXTRAS'))) {
+    define('TEST_SEARCH_SOLR_HOSTNAME', 'solr');
+    define('TEST_SEARCH_SOLR_INDEXNAME', 'test');
+    define('TEST_SEARCH_SOLR_PORT', 8983);
+
+    define('TEST_SESSION_REDIS_HOST', 'redis');
+    define('TEST_CACHESTORE_REDIS_TESTSERVERS', 'redis');
+
+    define('TEST_CACHESTORE_MONGODB_TESTSERVER', 'mongodb://mongo:27017');
+
+    define('TEST_CACHESTORE_MEMCACHED_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
+    define('TEST_CACHESTORE_MEMCACHE_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
+
+    define('TEST_LDAPLIB_HOST_URL', 'ldap://ldap');
+    define('TEST_LDAPLIB_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_LDAPLIB_BIND_PW', 'password');
+    define('TEST_LDAPLIB_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+
+    define('TEST_AUTH_LDAP_HOST_URL', 'ldap://ldap');
+    define('TEST_AUTH_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_AUTH_LDAP_BIND_PW', 'password');
+    define('TEST_AUTH_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+
+    define('TEST_ENROL_LDAP_HOST_URL', 'ldap://ldap');
+    define('TEST_ENROL_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_ENROL_LDAP_BIND_PW', 'password');
+    define('TEST_ENROL_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+}
+
+define('TEST_EXTERNAL_FILES_HTTP_URL', 'http://exttests');
+
+require_once(__DIR__ . '/lib/setup.php');
---
+++ run.sh
@@ -0,0 +1,358 @@
+#!/bin/bash
+set -u
+set -o pipefail
+
+CACHEDIR="/var/lib/jenkins/caches"
+export COMPOSERCACHE="${COMPOSERCACHE:-${CACHEDIR}/composer}"
+export CODEDIR="${CODEDIR:-${WORKSPACE}/moodle}"
+export OUTPUTDIR="${WORKSPACE}"/"${BUILD_ID}"
+export ENVIROPATH="${OUTPUTDIR}"/environment.list
+
+# Which PHP Image to use.
+export PHP_SERVER_DOCKER="${PHP_SERVER_DOCKER:-moodlehq/moodle-php-apache:7.1}"
+
+# Default type of test to run.
+# phpunit or behat.
+export TESTTORUN="${TESTTORUN:-phpunit}"
+
+# Default DB settings.
+# Todo: Tidy this up properly.
+export DBTYPE="${DBTYPE:-pgsql}"
+export DBUSER="${DBUSER:-moodle}"
+export DBPASS="${DBPASS:-moodle}"
+export DBHOST="${DBHOST:-${DBTYPE}}"
+export DBPORT="${DBPORT:-}"
+export DBNAME="${DBNAME:-}"
+export DBPREFIX="${DBPREFIX:-}"
+export DBTORUN="${DBTORUN:-}"
+
+# Test defaults
+export BROWSER="${BROWSER:-chrome}"
+export BEHAT_SUITE="${BEHAT_SUITE:-}"
+export BEHAT_TOTAL_RUNS="${BEHAT_TOTAL_RUNS:-3}"
+
+mkdir -p "${OUTPUTDIR}"
+rm -f "${ENVIROPATH}"
+touch "${ENVIROPATH}"
+
+# Select db to use today.
+# TODO Change this.
+if [ -n "$DBTORUN" ]; then
+    DBTORUN=(`echo ${DBTORUN}`);
+    # Find which db to run today.
+    dayoftheweek=`date +"%u"`
+    if [[ -z ${DBTORUN} ]]; then
+        dbtouse=pgsql
+    else
+        dbtouse=${DBTORUN[ $(( ${dayoftheweek} - 1 )) ]}
+    fi
+    echo "Running against ${dbtouse}"
+
+    eval dbtousedetails="(\"\${$dbtouse[@]}\")"
+
+    # Set db values.
+    for dbtouse in ${dbtousedetails[@]}
+      do
+      KEY=${dbtouse%%:*}
+      VALUE=${dbtouse#*:}
+      eval ${KEY}=${VALUE}
+    done
+
+    # Get total runs.
+    if [[ -z ${PROCESS} ]]; then
+        if [ -z "$RUN" ]; then
+            RUN=1
+        fi
+
+        if [ -z "$TOTAL_RUN" ]; then
+            TOTAL_RUNS=1
+        fi
+    else
+        RUN=`echo $PROCESS | cut -d '/' -f 1`
+        TOTAL_RUNS=`echo $PROCESS | cut -d '/' -f 2`
+    fi
+
+    # Dbtype and dbhost can't be guessed. enure we have one.
+    if [ -z "$dbtype" ] || [ -z "$dbhost" ]; then
+        echo "Dbtype or dbhost is not set."
+        exit 1
+    fi
+    DBTYPE=${dbtype}
+    DBHOST=${dbhost}
+    if [[ -z ${dbname} ]]; then
+        DBNAME=${SiteId}
+    else
+        DBNAME=${dbname}
+    fi
+    if [[ -z ${dbuser} ]]; then
+        DBUSER=moodle
+    else
+        DBUSER=${dbuser}
+    fi
+    if [[ -z ${dbpass} ]]; then
+        DBPASS=moodle
+    else
+        DBPASS=${dbpass}
+    fi
+    if [[ -z ${dbprefix} ]]; then
+        if [ "$TESTTORUN" == "phpunit" ]; then
+            DBPREFIX=t_
+        else
+            DBPREFIX=b_
+        fi
+    else
+        DBPREFIX=${dbprefix}
+    fi
+
+    if [ -n "$dbport" ]; then
+        DBPORT="--dbport=${dbport}"
+    else
+        DBPORT=''
+    fi
+else
+    if [ -z "$DBPREFIX" ]
+    then
+        if [ "$TESTTORUN" == "phpunit" ]
+        then
+          DBPREFIX=t_
+        else
+          DBPREFIX=b_
+        fi
+    fi
+fi
+
+if [ -z "$DBNAME" ]
+then
+  echo "No database name specified"
+  exit 1
+fi
+
+
+# Setup Environment
+UUID=$(uuid | sha1sum | awk '{print $1}')
+UUID=${UUID:0:16}
+echo "DBTYPE" >> "${ENVIROPATH}"
+echo "DBHOST" >> "${ENVIROPATH}"
+echo "DBPORT" >> "${ENVIROPATH}"
+echo "DBUSER" >> "${ENVIROPATH}"
+echo "DBPASS" >> "${ENVIROPATH}"
+echo "DBNAME" >> "${ENVIROPATH}"
+echo "PREFIX" >> "${ENVIROPATH}"
+echo "BROWSER" >> "${ENVIROPATH}"
+echo "WEBSERVER" >> "${ENVIROPATH}"
+
+echo "============================================================================"
+echo "== Job summary:"
+echo "== Workspace: ${WORKSPACE}"
+echo "== Build Id: ${BUILD_ID}"
+echo "== Output directory: ${OUTPUTDIR}"
+echo "== UUID: ${UUID}"
+echo "== Container prefix: ${UUID}"
+echo "== DBTYPE: ${DBTYPE}"
+echo "== DBHOST: ${DBHOST}"
+echo "== DBPORT: ${DBPORT}"
+echo "== DBUSER: ${DBUSER}"
+echo "== DBPASS: ${DBPASS}"
+echo "== DBNAME: ${DBNAME}"
+echo "== PREFIX: ${DBPREFIX}"
+echo "== Environment: ${ENVIROPATH}"
+echo "============================================================================"
+
+# Setup the image cleanup.
+function finish {
+  echo "Stopping all docker images for ${UUID}"
+  for image in `docker ps -a -q --filter name=${UUID}`
+  do
+      docker stop $image
+  done
+}
+trap finish EXIT
+
+function ctrl_c() {
+  echo "============================================================================"
+  echo "Job was cancelled at user request"
+  echo "============================================================================"
+  exit 255
+}
+trap ctrl_c INT
+
+
+if [ "$TESTTORUN" == "behat" ]
+then
+  SELNAME=sel"${UUID}"
+  if [ "$BROWSER" == "chrome" ]
+  then
+    SHMMAP="-v /dev/shm:/dev/shm"
+
+    docker run \
+      --network nightly \
+      --name ${SELNAME} \
+      --detach \
+      $SHMMAP \
+      -v "${CODEDIR}":/var/www/html \
+      -p 5900:5900 \
+      selenium/standalone-chrome-debug
+  else
+    SHMMAP=''
+    docker run \
+      --network nightly \
+      --name ${SELNAME} \
+      --detach \
+      $SHMMAP \
+      -v "${CODEDIR}":/var/www/html \
+      selenium/standalone-firefox:2.53.1
+  fi
+
+  export SELENIUMURL="http://${SELNAME}:4444"
+  echo SELENIUMURL >> "${ENVIROPATH}"
+fi
+
+# Start the test server.
+export WEBSERVER=run"${UUID}"
+docker run \
+  --network nightly \
+  --name "${WEBSERVER}" \
+  --detach \
+  --env-file "${ENVIROPATH}" \
+  -v "${COMPOSERCACHE}:/root/.composer:rw" \
+  -v "${OUTPUTDIR}":/shared \
+  ${PHP_SERVER_DOCKER}
+
+# Copy code in place.
+docker cp "${CODEDIR}"/. "${WEBSERVER}":/var/www/html
+
+# Copy the config.php in place
+docker cp /store/scripts/configs/config.template.php "${WEBSERVER}":/var/www/html/config.php
+
+# Setup the DB.
+if [ "$TESTTORUN" == "behat" ]
+then
+  # Setup Composer
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/behat/cli/init.php
+
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/behat/cli/util.php --drop
+
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/behat/cli/util.php --drop -j=10
+
+  # Init tables.
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/behat/cli/init.php \
+      -j="${BEHAT_TOTAL_RUNS}"
+else
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/phpunit/cli/init.php \
+      --force
+fi
+
+# Run the test.
+if [ "$TESTTORUN" == "behat" ]
+then
+  BEHAT_FORMAT_DOTS="--format=moodle_progress --out=std"
+  BEHAT_FORMAT_PRETTY="--format=pretty --out=/shared/pretty{runprocess}.txt --replace={runprocess}"
+  BEHAT_FORMAT_JUNIT="--format=junit --out=/shared/log{runprocess}.junit --replace={runprocess}"
+  MOODLE_VERSION=$(grep "\$branch" "${CODEDIR}"/version.php | sed "s/';.*//" | sed "s/^\$.*'//")
+
+  if [ -n "$BEHAT_SUITE" ]
+  then
+    BEHAT_INIT_SUITE="-a=${BEHAT_SUITE}"
+    BEHAT_RUN_SUITE="--suite=${BEHAT_SUITE}"
+  else
+    BEHAT_INIT_SUITE=""
+    BEHAT_RUN_SUITE=""
+  fi
+
+  docker exec -t "${WEBSERVER}" \
+    php admin/tool/behat/cli/run.php \
+      ${BEHAT_INIT_SUITE} \
+      ${BEHAT_FORMAT_DOTS} \
+      ${BEHAT_FORMAT_PRETTY} \
+      ${BEHAT_FORMAT_JUNIT} \
+      ${BEHAT_RUN_SUITE}
+  EXITCODE=$?
+
+  # Re-run failed scenarios, to ensure they are true fails.
+  # If we are running a single, we don't need to check for each run.
+  if [ ${EXITCODE} -eq 0 ]
+  then
+    echo "============================================================================"
+    echo "== Exit code: ${EXITCODE}"
+    echo "== Test result: Pass"
+    echo "============================================================================"
+  else
+    if [ "$BEHAT_TOTAL_RUNS" -le 1 ]
+    then
+      if [ "$MOODLE_VERSION" -ge "32" ]
+      then
+        CONFIGPATH="/var/www/behatdata/behatrun/behat/behat.yml"
+      else
+        CONFIGPATH="/var/www/behatdata/behat/behat.yml"
+      fi
+
+      echo "---Running behat again for failed steps---"
+      CMD="vendor/bin/behat"
+      CMD="${CMD} --config ${CONFIGPATH}"
+      CMD="${CMD} ${BEHAT_FORMAT_DOTS}"
+      CMD="${CMD} --format=pretty --out=/shared/pretty_rerun.txt"
+      CMD="${CMD} --format=junit --out=/shared/log_rerun.junit"
+      CMD="${CMD} ${BEHAT_RUN_SUITE}"
+      CMD="${CMD} --verbose"
+      CMD="${CMD} --rerun"
+
+      docker exec -t "${WEBSERVER}" ${CMD}
+      NEWEXITCODE=$?
+    else
+      NEWEXITCODE=0
+      for RUN in `seq 1 "${BEHAT_TOTAL_RUNS}"`
+      do
+        # Check is the previous build failed
+        status=$((1 << $RUN-1))
+        CURRENTRUNEXITCODE=$(($status & $EXITCODE))
+        if [ $CURRENTRUNEXITCODE -eq 0 ]
+        then
+          continue
+        fi
+
+        if [ "$MOODLE_VERSION" -ge "32" ]
+        then
+          CONFIGPATH="/var/www/behatdata/behatrun${RUN}/behat/behat.yml"
+        else
+          CONFIGPATH="/var/www/behatdata${RUN}/behat/behat.yml"
+        fi
+
+        echo "---Running behat again for failed steps on process ${RUN}  ---"
+
+        docker exec -t "${WEBSERVER}" [ ! -L "behatrun{$RUN}" ] && docker exec -t "${WEBSERVER}" ln -s /var/www/html "behatrun${RUN}"
+        CMD="vendor/bin/behat"
+        CMD="${CMD} --config ${CONFIGPATH}"
+        CMD="${CMD} ${BEHAT_FORMAT_DOTS}"
+        CMD="${CMD} --format=pretty --out=/shared/pretty${RUN}_rerun.txt"
+        CMD="${CMD} --format=junit --out=/shared/log${RUN}_rerun.junit"
+        CMD="${CMD} ${BEHAT_RUN_SUITE}"
+        CMD="${CMD} --verbose"
+        CMD="${CMD} --rerun"
+
+        docker exec -t "${WEBSERVER}" ${CMD}
+        NEWEXITCODE=$(($NEWEXITCODE + $?))
+      done
+    fi
+    EXITCODE=${NEWEXITCODE}
+  fi
+
+else
+  docker exec -t "${WEBSERVER}" \
+    php vendor/bin/phpunit \
+      --log-junit "/shared/log.junit" \
+      --verbose
+  EXITCODE=$?
+
+fi
+
+echo "============================================================================"
+echo "== Exit summary":
+echo "== Exit code: ${EXITCODE}"
+echo "============================================================================"
+
+exit $EXITCODE
