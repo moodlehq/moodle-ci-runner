c07c5a379e6cbb1f77b43bef5267eadc50d2391e
Andrew Nicols <andrew@nicols.co.uk>
2018-02-16 09:21:22 +0800

    [general] Archive old nightly scripts
    The Old Nightly Infrastructure is dead.
    
    Long live the Old Nightly Infrastructure.

--- dockercleaner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#############################################
-# This script will remove any existing docker
-# instances which have existed but not removed
-#############################################
-
-docker rm $(docker ps -a --filter 'status=exited' -q)
--- maintenance/backup_jenkins_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-backupdate=`date +%Y%M%d_%H%m%S`
-backupfile="/store/jenkins/backups/config_backup_$backupdate.tgz"
-
-echo "Backing up /var/lib/jenkins to $backupfile"
-find /var/lib/jenkins -iname 'config.xml' | tar -czf "$backupfile" --files-from -
