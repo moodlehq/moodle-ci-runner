44c4e60038cb731a694fda364a6d9731933e128b
Sara Arjona <sara@moodle.com>
2019-05-31 12:28:21 +0200

    runner: Add PLUGINSTOINSTALL variable to install external plugins

--- runner/master/run.sh
+++ runner/master/run.sh
@@ -27,6 +27,44 @@ export COMPOSERCACHE="${COMPOSERCACHE:-${CACHEDIR}/composer}"
 export CODEDIR="${CODEDIR:-${WORKSPACE}/moodle}"
 export OUTPUTDIR="${WORKSPACE}"/"${BUILD_ID}"
 export ENVIROPATH="${OUTPUTDIR}"/environment.list
+# The PLUGINSTOINSTALL variable could be set to install external plugins in the CODEDIR folder. The following information is needed
+# for each plugin: gitrepo, folder and branch (optional). The plugin fields should be separated by "|" and each plugin should
+# be separated using ";": "gitrepoplugin1|gitfolderplugin1|gitbranchplugin1;gitrepoplugin2|gitfolderplugin2|gitbranchplugin2[...]"
+# Example: "https://github.com/moodlehq/moodle-local_mobile.git|local/mobile|MOODLE_37_STABLE;git@github.com:jleyva/moodle-block_configurablereports.git|blocks/configurable_reports"
+export PLUGINSTOINSTALL="${PLUGINSTOINSTALL:-}"
+
+if [ -n "$PLUGINSTOINSTALL" ];
+then
+  echo ">>> startsection Download external plugins <<<"
+  echo "============================================================================"
+  # Create a temporary folder.
+  PLUGINSTMPDIR=$(mktemp -d -t ci-$(date +%Y-%m-%d-%H-%M-%S)-XXXXXXXXXX)
+  # Download all the plugins in a temporary folder.
+  IFS=';' read -ra PLUGINS <<< "$PLUGINSTOINSTALL"
+  for PLUGIN in "${PLUGINS[@]}";
+  do
+    if  [ -n "$PLUGIN" ]
+    then
+      PLUGINGITREPO=$(echo "$PLUGIN" | cut -f1 -d'|')
+      PLUGINFOLDER=$(echo "$PLUGIN" | cut -f2 -d'|')
+      PLUGINBRANCH=$(echo "$PLUGIN" | cut -f3 -d'|')
+
+      if [ -n "${PLUGINBRANCH}" ]
+      then
+        # Only download this branch.
+        PLUGINBRANCH="-b ${PLUGINBRANCH} --single-branch"
+      fi
+
+      # Clone the plugin repository in the defined folder.
+      git clone ${PLUGINBRANCH} ${PLUGINGITREPO} "${PLUGINSTMPDIR}/${PLUGINFOLDER}"
+      echo
+    fi
+  done
+  unset IFS
+  echo "============================================================================"
+  echo ">>> stopsection <<<"
+  echo
+fi
 
 # Which PHP Image to use.
 export PHP_VERSION="${PHP_VERSION:-7.1}"
@@ -470,6 +508,11 @@ docker run \
 # Copy code in place.
 echo "== Copying code in place"
 docker cp "${CODEDIR}"/. "${WEBSERVER}":/var/www/html
+if [ -v PLUGINSTMPDIR ]
+then
+  echo "== Copying external plugins in place"
+  docker cp "${PLUGINSTMPDIR}"/. "${WEBSERVER}":/var/www/html
+fi
 
 # Copy the config.php in place
 echo "== Copying configuration"
@@ -741,6 +784,13 @@ echo "==========================================================================
 
 docker exec -t "${WEBSERVER}" \
   chown -R "${UID}:${GROUPS[0]}" /shared
+
+# Delete the temporary folder.
+if [ -v PLUGINSTMPDIR ]
+then
+  echo "== Removing temporary folder."
+  rm -rf ${PLUGINSTMPDIR}
+fi
 
 echo "============================================================================"
 echo ">>> stopsection <<<"
